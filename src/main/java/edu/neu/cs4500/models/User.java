8f98668c076d20c9225ea05bbc18cfb15702667c
catherinemclean <catmclean@verizon.net>
2019-01-22 12:10:04 -0500

    Added the new class 'User' under models

---
+++ src/main/java/edu/neu/cs4500/models/User.java
@@ -0,0 +1,11 @@
+package edu.neu.cs4500.models;
+
+public class User {
+
+	private Integer id;
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+
+}
