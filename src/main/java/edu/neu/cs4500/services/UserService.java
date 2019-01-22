d87270c9aa80f0d29f29ee09167fd96c38cf5078
catherinemclean <catmclean@verizon.net>
2019-01-22 13:42:45 -0500

    Add a method that maps all the valid users to the /api/user link

--- src/main/java/edu/neu/cs4500/services/UserService.java
+++ src/main/java/edu/neu/cs4500/services/UserService.java
@@ -3,6 +3,7 @@ package edu.neu.cs4500.services;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.neu.cs4500.models.User;
@@ -13,5 +14,10 @@ public class UserService {
 	static {
 		users.add(new User(123, "alice", "alice", "Alice", "Wonderland"));
 		users.add(new User(234, "bob", "bob", "Bob", "Marley"));
+	}
+	
+	@GetMapping("/api/user")
+	public List<User> findAllUsers() {
+		return users;
 	}
 }
