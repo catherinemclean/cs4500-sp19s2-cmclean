fd10b670af0a2d7faa4824c4e6750462f9481a89
catherinemclean <catmclean@verizon.net>
2019-01-22 13:46:16 -0500

    Add a method that maps each specific valid user to the /api/user/{userId} link

--- src/main/java/edu/neu/cs4500/services/UserService.java
+++ src/main/java/edu/neu/cs4500/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.neu.cs4500.models.User;
@@ -15,9 +16,18 @@ public class UserService {
 		users.add(new User(123, "alice", "alice", "Alice", "Wonderland"));
 		users.add(new User(234, "bob", "bob", "Bob", "Marley"));
 	}
-	
 	@GetMapping("/api/user")
 	public List<User> findAllUsers() {
 		return users;
+	}
+	@GetMapping("/api/user/{userId}")
+	public User findUserById(
+		@PathVariable("userId") Integer id) {
+		for(User user: users) {
+			if(user.getId() == id) {
+				return user;
+			}
+		}
+		return null;
 	}
 }
